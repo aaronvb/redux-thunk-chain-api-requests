506349991c093d18d2593f58ed5f118afcbedcbe
Aaron Van Bokhoven <bokhoven@gmail.com>
2018-01-24 21:13:45 -1000

    Start templating the index

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>React Issues</h1>
+        <a href="https://github.com/facebook/react/issues">
+          https://github.com/facebook/react/issues
+        </a>
       </div>
     );
   }
