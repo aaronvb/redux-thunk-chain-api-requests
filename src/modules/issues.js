5d28b7f3a32a68ca31ac9de76ba1d0548203c920
Aaron Van Bokhoven <bokhoven@gmail.com>
2018-01-25 22:02:45 -1000

    Add request individual issue and update state hash table

--- src/modules/issues.js
+++ src/modules/issues.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 export const ISSUES_REQUESTED = 'issues/ISSUES_REQUESTED';
 export const ISSUES_RECEIVED = 'issues/ISSUES_RECEIVED';
 
-export const ISSUE_REQUESTED = 'issues/ISSUE_REQUESTED';
 export const ISSUE_RECEIVED = 'issues/ISSUE_RECEIVED';
 
 const initialState = {
@@ -30,6 +29,10 @@ export default (state = initialState, action) => {
     case ISSUE_RECEIVED:
       return {
         ...state,
+        issuesByHash: {
+          ...state.issuesByHash,
+          [action.id]: action.payload,
+        },
       };
 
     default:
@@ -37,6 +40,10 @@ export default (state = initialState, action) => {
   }
 };
 
+// Request issues using axios
+// Request: GET
+// Params: none
+// Return: Array
 export const requestIssues = () => {
   return dispatch => {
     dispatch({
@@ -56,6 +63,31 @@ export const requestIssues = () => {
 
 const fetchIssues = () => {
   const url = 'https://api.github.com/repos/facebook/react/issues';
+
+  return axios.get(url);
+};
+
+// Request individual issue using axios
+// Request: GET
+// Params: issue number
+// Return: Object
+export const requestIssueHash = issueNumber => {
+  return dispatch => {
+    return fetchIssue(issueNumber)
+      .then(resp => resp.data)
+      .then(data =>
+        dispatch({
+          type: ISSUE_RECEIVED,
+          id: data.id,
+          payload: data,
+        }),
+      );
+  };
+};
+
+const fetchIssue = issueNumber => {
+  const url =
+    '/https://api.github.com/repos/facebook/react/issues/' + issueNumber;
 
   return axios.get(url);
 };
