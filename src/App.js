5b76b75a646271bbd481b373f2d837cde5791207
Aaron Van Bokhoven <bokhoven@gmail.com>
2018-01-29 21:26:28 -1000

    Render issue comments

--- src/App.js
+++ src/App.js
@@ -13,10 +13,37 @@ class App extends Component {
     const issuesById = this.props.issuesById;
 
     const issues = issuesById.map(issue => (
-      <li key={issue.id}>{issue.title}</li>
+      <li key={issue.id}>
+        <strong>#{issue.number}</strong>{' '}
+        <a href={issue.html_url}>{issue.title}</a>
+        <div className="issueComments">{this.renderIssueComments(issue)}</div>
+      </li>
     ));
 
     return issues;
+  }
+
+  renderIssueComments(issue) {
+    if (this.props.issueCommentsByHash[issue.number]) {
+      const commentsArr = this.props.issueCommentsByHash[issue.number];
+      return <ul>{this.renderIssueComment(commentsArr)}</ul>;
+    } else {
+      return <span>Loading...</span>;
+    }
+  }
+
+  renderIssueComment(commentsArr) {
+    if (commentsArr.length === 0) {
+      return <em>No comments.</em>;
+    } else {
+      const comments = commentsArr.map(comment => (
+        <li key={comment.id}>
+          <strong>{comment.user.login}</strong>:
+          <div>{comment.body}</div>
+        </li>
+      ));
+      return comments;
+    }
   }
 
   render() {
